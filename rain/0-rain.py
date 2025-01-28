243194a9f77d10896c85956f195851745e0942d6
kambaikariuki <kariukikamba@gmail.com>
2025-01-28 13:56:25 +0200

    pep8 correction

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,11 +5,11 @@
 def rain(walls):
     if not walls:
         return 0
-    
+
     left, right = 0, len(walls) - 1
     left_max, right_max = walls[left], walls[right]
     water_trapped = 0
-    
+
     while left < right:
         if walls[left] < walls[right]:
             if walls[left] >= left_max:
@@ -23,5 +23,5 @@ def rain(walls):
             else:
                 water_trapped += right_max - walls[right]
             right -= 1
-    
+
     return water_trapped
