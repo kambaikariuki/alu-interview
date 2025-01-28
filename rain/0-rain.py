b60dcbb43211903009216e0ccb05447af0b9f2ae
kambaikariuki <kariukikamba@gmail.com>
2025-01-28 13:53:10 +0200

    task 0

---
+++ rain/0-rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Rain function'''
+
+
+def rain(walls):
+    if not walls:
+        return 0
+    
+    left, right = 0, len(walls) - 1
+    left_max, right_max = walls[left], walls[right]
+    water_trapped = 0
+    
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water_trapped += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water_trapped += right_max - walls[right]
+            right -= 1
+    
+    return water_trapped
